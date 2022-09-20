a64cf2f4d4f2e0a75fa883bbe153a23643c405e6
João Alonso <joao.alonso.s@gmail.com>
2022-09-19 21:02:11 -0300

    worked in auth and token endpoint tests

--- test/auth_test.py
+++ test/auth_test.py
@@ -2,24 +2,47 @@ import requests
 import pytest
 import json
 
-url = ''
+class TestAuth:
+    url = ''
 
-def test_create_token_with_username_success():
-    r = requests.get(f'{url}/token', json= {'login':'', 'pswd':''})
-    print(r.content)
-    assert r.status_code == 200
+    successfull_user_json = {'login':'', 'pswd':''}
+    unsuccessfull_user_json = {'login':'', 'pswd':''}
+    
+    def test_create_token_with_username_success(self):
+        response = requests.get(f'{self.url}/token', json= self.successfull_user_json)
+        assert response.status_code == 200
 
-def test_create_with_token_with_email_success():
-    r = requests.get(f'{url}/token', json= {'login':'', 'pswd':''})
-    print(r.content)
-    assert r.status_code == 200
+    def test_create_with_token_with_email_success(self):
+        response = requests.get(f'{self.url}/token', json= self.successfull_user_json)
+        assert response.status_code == 200
 
-def test_create_token_with_username_pswd_error():
-    r = requests.get(f'{url}/token', json= {'login':'', 'pswd':''})
-    r_json = json.loads(r.json())
-    assert r_json['error'] == 'Invalid user and\or password'
 
-def test_create_token_with_email_pswd_error():
-    r = requests.get(f'{url}/token', json= {'login':'', 'pswd':''})
-    r_json = json.loads(r.json())
-    assert r_json['error'] == 'Invalid user and\or password'
+    def test_create_token_with_username_pswd_error(self):
+        response = requests.get(f'{self.url}/token', json= self.unsuccessfull_user_json)
+        response_json = json.loads(response.json())
+        assert response.status_code == 202 and response_json['error'] == 'Invalid user and\\or password'
+  
+    
+
+    def test_create_token_with_email_pswd_error(self):
+        response = requests.get(f'{self.url}/token', json= self.unsuccessfull_user_json)
+        response_json = json.loads(response.json())
+        assert response.status_code == 202 and response_json['error'] == 'Invalid user and\\or password'
+  
+
+    def test_auth_success(self):
+        response = requests.get(f'{self.url}/token', json= self.successfull_user_json)
+        token = json.loads(response.json())['token']
+        response = requests.get(f'{self.url}/auth', headers = {'token' : token})
+        assert response.status_code == 200
+
+    def test_auth_no_token(self):
+        response = requests.get(f'{self.url}/auth')
+        response_json = json.loads(response.json())
+        assert response.status_code == 202 and response_json['error'] == 'No token found'
+    
+    def test_auth_invalid_token(self):
+        response = requests.get(f'{self.url}/auth', headers = {'token' : 'NotAToken'})
+        assert response.status_code == 500
+    
+        
