9c6bac0b0c37353868a3d3a39740b11d90358a46
João Alonso <joao.alonso.s@gmail.com>
2023-05-04 20:34:51 -0300

    Remove tests maded the wrong way

---
+++ test/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestApp(unittest.TestCase):
+    
+    def test_assert_true(self):
+        self.assertTrue(True)
+
+    def test_assert_false(self):
+        self.assertTrue(False)
