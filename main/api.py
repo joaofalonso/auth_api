ab561af531de4b626018b1e5015f406c9d955074
João Alonso <joao.alonso.s@gmail.com>
2023-05-04 21:43:07 -0300

    reset

--- main/api.py
+++
@@ -1,5 +0,0 @@
-from flask_restx import Api
-from flask import Flask
-
-app = Flask(__name__)
-api = Api(app, title='Auth Api', description='Implementation of Authentication using Flask and JWT')
--- main/resources/exception/resource_exception.py
+++
@@ -1,5 +0,0 @@
-
-class ResourceException(Exception):
-
-    def __init__(self, message):
-        super().__init__(message)
